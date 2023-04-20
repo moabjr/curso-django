0e5c6fa5c9fb9049d33a33bbc4ed81f7cc5d0ddf
Moab de Carvalho Lima Júnior <moabjr2@gmail.com>
2023-04-20 20:23:41 -0300

    atualizações 20-04-23

--- recipes/views.py
+++ recipes/views.py
@@ -4,4 +4,13 @@ from django.shortcuts import render
 
 # Criando as view do Projeto Recipes.
 def home(request):
-    return render(request, 'recipes/pages/home.html')
+    return render(request, 'recipes/pages/home.html', context={
+        'name': 'Moab Lima',
+    })
+
+
+# Criando as view do Projeto Recipes.
+def recipe(request, id):
+    return render(request, 'recipes/pages/recipe-view.html', context={
+        'name': 'Moab Lima',
+    })
